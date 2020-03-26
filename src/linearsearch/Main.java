019315263b437a6d3e8ea8408471cbb177e8c842
dbc2201 <divyanshb22@gmail.com>
2020-03-26 09:19:52 +0530

    add JavaDoc comment for class

---
+++ src/linearsearch/Main.java
@@ -0,0 +1,14 @@
+/*
+ *   Created by IntelliJ IDEA Ultimate, 2020
+ *   User: dbc2201
+ *   Date: 26/03/20
+ *   Time: 9:18 AM
+ */
+
+package linearsearch;
+
+/**
+ * This class will be used to demonstrate linear search.
+ */
+public class Main {
+}
