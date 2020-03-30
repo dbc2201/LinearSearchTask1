547bd2874ff873a8bc8fe317d667b81507d747d2
dbc2201 <divyanshb22@gmail.com>
2020-03-30 13:10:57 +0530

    ask user for number of elements in the array

--- src/binarysearch/Main.java
+++ src/binarysearch/Main.java
@@ -9,9 +9,14 @@ package binarysearch;
 
 import exceptions.NumberNotFoundInArrayException;
 
+import java.util.Scanner;
+
 public class Main {
     public static void main(String[] args) {
-
+        System.out.println("How many elements would you like in the array?: ");
+        final Scanner scanner = new Scanner(System.in);
+        int numberOfElements = scanner.nextInt();
+        scanner.close();
     }
 
     /**
