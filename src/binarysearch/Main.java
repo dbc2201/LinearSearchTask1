05783fa19b9391914aa45e2f761b05dcf9f42b65
dbc2201 <divyanshb22@gmail.com>
2020-03-30 13:04:52 +0530

    add binarySearch method

--- src/binarysearch/Main.java
+++ src/binarysearch/Main.java
@@ -11,4 +11,9 @@ public class Main {
     public static void main(String[] args) {
 
     }
+
+    private static int binarySearch(int[] array, int number) {
+        int response = -1;
+        return response;
+    }
 }
