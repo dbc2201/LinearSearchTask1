35d0e0bb0a4042ce52a438eb13785494e5db8687
dbc2201 <divyanshb22@gmail.com>
2020-04-01 10:45:42 +0530

    create a Scanner class object

--- src/main/Main.java
+++ src/main/Main.java
@@ -7,8 +7,12 @@
 
 package main;
 
+import java.util.Scanner;
+
 public class Main {
     public static void main(String[] args) {
         System.out.println("How many elements would you like in the array?: ");
+        final Scanner scanner = new Scanner(System.in);
+        scanner.close();
     }
 }
