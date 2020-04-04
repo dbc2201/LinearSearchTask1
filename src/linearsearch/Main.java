782068308c4f026ce37540e9888dc5a96b82d1cb
dbc2201 <divyanshb22@gmail.com>
2020-04-04 17:56:25 +0530

    delete the pseudo main method

--- src/linearsearch/Main.java
+++ src/linearsearch/Main.java
@@ -9,33 +9,10 @@ package linearsearch;
 
 import exceptions.NumberNotFoundInArrayException;
 
-import java.util.Arrays;
-import java.util.Scanner;
-
 /**
  * This class will be used to demonstrate linear search.
  */
 public class Main {
-    public static void main(String[] args) {
-        System.out.println("How many elements do you want in the array?");
-        final Scanner scanner = new Scanner(System.in);
-        int numberOfElements = scanner.nextInt();
-        int[] numbers = new int[numberOfElements];
-        // set random values in the array
-        for (int i = 0; i < numbers.length; i++) {
-            numbers[i] = (int) (Math.random() * 100);
-        }
-        System.out.println(Arrays.toString(numbers));
-        System.out.println("Enter the number to search for");
-        int numberToSearchFor = scanner.nextInt();
-        try {
-            System.out.println(findNumberInIntegerArray(numbers, numberToSearchFor));
-        } catch (NumberNotFoundInArrayException e) {
-            System.out.println("Number not found in the Array!");
-        }
-        scanner.close();
-    }
-
     /**
      * Finds an integer number in a given integer array.
      *
